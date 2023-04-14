a6f6514c15f244d1d12f80db6e3c242204d6ba69
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-14 19:04:48 +0900

    Use Optional Chaining

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "Tom",
+  hobbies: ["Movie", "Game"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,6 +24,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="28px" />
       <button onClick={onClickFetchData}>Get Data</button>
       {todos.map((todo) => (
